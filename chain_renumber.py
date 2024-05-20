877665a57c1beb6fd3225fa584f3b4c2e193e5c7
Sunandan Mukherjee <sunandan2@gmail.com>
2024-05-20 13:32:13 +0200

    Update chain_renumber.py
    argparse and inline option added

--- chain_renumber.py
+++ chain_renumber.py
@@ -1,46 +1,82 @@
 #!/usr/bin/env python
 
-import sys
+import argparse
+from typing import List, Optional
 
-def renum_pdb(pdb, start=None):
+def renum_pdb(pdb: List[str], start: Optional[int] = None) -> List[str]:
+    """
+    Renumber residues in a PDB file starting from a given number.
+
+    Args:
+        pdb (List[str]): List of lines from a PDB file containing ATOM records.
+        start (Optional[int]): The starting residue number. If None, starts from 1.
+
+    Returns:
+        List[str]: List of renumbered PDB lines.
+    """
     curr_res = set()
     renumbered = []
     
-    if start:
-        start = start-1
-    else:
-        start = 0
+    start = (start - 1) if start else 0
     
     for line in pdb:
         res_num = None
         res_tag = line[17:27]
         
         if res_tag in curr_res:
-            res_num = str("%4d" %start)
-            renumbered.append(line[:22]+res_num+line[26:])
+            res_num = f"{start:4d}"
+            renumbered.append(line[:22] + res_num + line[26:])
         else:
             curr_res.add(res_tag)
             start += 1
-            res_num = str("%4d" %start)
-            renumbered.append(line[:22]+res_num+line[26:])
+            res_num = f"{start:4d}"
+            renumbered.append(line[:22] + res_num + line[26:])
             
     return renumbered 
-        
-def get_atoms(infile):
+
+def get_atoms(infile: str) -> List[str]:
+    """
+    Extract ATOM lines from a PDB file.
+
+    Args:
+        infile (str): Path to the input PDB file.
+
+    Returns:
+        List[str]: List of ATOM lines from the PDB file.
+    """
     coordinates = []
-    for line in open(infile).readlines():
-        if line.startswith("ATOM"):
-            coordinates.append(line)
+    with open(infile) as f:
+        for line in f:
+            if line.startswith("ATOM"):
+                coordinates.append(line)
     return coordinates
 
 def main():
-    infile = sys.argv[1]
-    outfile = open(sys.argv[2], 'w')
+    """
+    Main function to parse command-line arguments and renumber residues in a PDB file.
+    """
+    parser = argparse.ArgumentParser(description="Renumber PDB file residues.")
+    parser.add_argument("infile", help="Input PDB file")
+    parser.add_argument("outfile", nargs="?", help="Output PDB file (not valid if --inline is used)")
+    parser.add_argument("--start", type=int, default=1, help="Starting residue number (default: 1)")
+    parser.add_argument("--inline", "-i", action="store_true", help="Edit the input file inline")
+
+    args = parser.parse_args()
+
+    if args.inline and args.outfile:
+        parser.error("The --inline option cannot be used with an output file.")
+
+    atoms = get_atoms(args.infile)
+    renumbered_atoms = renum_pdb(atoms, args.start)
     
-    for line in renum_pdb(get_atoms(infile)):
-        outfile.write(line)
-    outfile.close()
+    if args.inline:
+        outfile_path = args.infile
+    else:
+        outfile_path = args.outfile
+
+    with open(outfile_path, 'w') as f:
+        for line in renumbered_atoms:
+            f.write(line)
         
 if __name__ == "__main__":
     main()
-    
